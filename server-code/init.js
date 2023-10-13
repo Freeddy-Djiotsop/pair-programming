677d05f518404c010661f820f9c9e341e69cb932
Freeddy Djiotsop <djiotsopfreeddy@gmail.com>
2023-10-13 13:13:10 +0200

    Grundlage für Quellcode in DB speichern

---
+++ server-code/init.js
@@ -0,0 +1,25 @@
+const { codeSourceBasic, fileExtensions } = require("./helper/codeBasic");
+const { CodeBasic } = require("./helper/mongodb");
+
+for (const extension of fileExtensions) {
+  const tmp = new CodeBasic({
+    extension,
+    content: codeSourceBasic[extension],
+  });
+
+  tmp
+    .save()
+    .then(() => {
+      console.log(`${extension} Grundlage gespeichert.`);
+    })
+    .catch((error) => {
+      if (error.code === 11000 || error.code === 11001) {
+        console.log(`${extension} Grundlage schon in der DB gespeichert.`);
+      } else {
+        console.error(
+          `Fehler beim Speichern der ${extension} Grundlage:\n`,
+          error
+        );
+      }
+    });
+}
