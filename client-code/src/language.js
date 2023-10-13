065bb6a7008cdad5e0b6c3bf7c199a135014245a
Freeddy Djiotsop <djiotsopfreeddy@gmail.com>
2023-10-13 12:56:15 +0200

    unterstütze Programmiersprachen Definition

---
+++ client-code/src/language.js
@@ -0,0 +1,34 @@
+export const languages = {
+  c: {
+    name: "c",
+    extension: "c",
+  },
+  cpp: {
+    name: "cpp",
+    extension: "cpp",
+  },
+  css: {
+    name: "css",
+    extension: "css",
+  },
+  html: {
+    name: "html",
+    extension: "html",
+  },
+  js: {
+    name: "javascript",
+    extension: "js",
+  },
+  php: {
+    name: "php",
+    extension: "php",
+  },
+  py: {
+    name: "python",
+    extension: "py",
+  },
+  txt: {
+    name: "txt",
+    extension: "txt",
+  },
+};
