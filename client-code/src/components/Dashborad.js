1102e49f11ac2b14c644d049e711769022cab3b6
Freeddy Djiotsop <djiotsopfreeddy@gmail.com>
2023-10-13 12:45:42 +0200

    Erstellen von Projekte hinzufügen

--- client-code/src/components/Dashborad.js
+++ client-code/src/components/Dashborad.js
@@ -1,18 +1,54 @@
 import { useEffect, useState } from "react";
 import "./styles/dashboard.css";
 import Modal from "react-modal";
+import { useForm } from "react-hook-form";
 import axios from "../api/axios";
 import { useAuth } from "./Auth";
+import { languages } from "../language";
 import { notierror, notisuccess } from "../toast";
 import { useNavigate } from "react-router-dom";
+
+Modal.setAppElement("#root");
 
 export default function Dashboard() {
   const auth = useAuth();
   const navigate = useNavigate();
   const [projects, setProjects] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loadProject, setLoadProject] = useState(false);
+
+  const onSubmit = async (data) => {
+    const { name, language, description } = data;
+    try {
+      await axios.post("project", {
+        email: auth.user.email,
+        name,
+        description,
+        extension: languages[language].extension,
+      });
+      closeModal();
+      setLoadProject(!loadProject);
+      notisuccess(`${name} erfolgreich erstellt`);
+    } catch (error) {
+      notierror(error.response.data.error.message);
+    }
+  };
 
   const handleClick = (id) => {
     navigate(`/project/${id}`, { replace: true });
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   useEffect(() => {
@@ -33,11 +69,16 @@ export default function Dashboard() {
           `Fehler beim Abrufen der Projektdaten: ${error.response.data.error.message}`
         );
       });
-  }, []);
+  }, [loadProject]);
 
   return (
     <>
       <div className="dashboard">
+        <div className="dashboard-left-panel">
+          <a className="dashboard-button-projekt" onClick={openModal}>
+            Neues Projekt
+          </a>
+        </div>
         <div className="dashboard-center-panel">
           <h1>Dashboard</h1>
           <ul>
@@ -49,6 +90,50 @@ export default function Dashboard() {
           </ul>
         </div>
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        appElement={document.getElementById("root")}
+        className="custom-modal"
+        overlayClassName="custom-overlay"
+        onRequestClose={closeModal}
+        contentLabel="Neues Projekt erstellen"
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="dashboard-input-box">
+            <input
+              type="text"
+              placeholder="Enter a name"
+              autoFocus
+              {...register("name", { required: true })}
+            />
+          </div>
+          <div className="dashboard-text-area">
+            <textarea
+              placeholder="Optional ReadMe"
+              {...register("description")}
+            />
+          </div>
+          <div className="dashboard-selection">
+            <label htmlFor="language" className="label">
+              Choose a language:
+            </label>
+            <select
+              className="select"
+              {...register("language", { required: true })}
+            >
+              {Object.keys(languages).map((key) => (
+                <option key={key} value={key}>
+                  {languages[key].extension}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="dasboard-button">
+            <button onClick={closeModal}>Abbrechen</button>
+            <button type="submit">Erstellen</button>
+          </div>
+        </form>
+      </Modal>
     </>
   );
 }
