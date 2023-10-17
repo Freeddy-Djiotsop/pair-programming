3529646c934bcd765e7ce514be7dba6cba7b84af
Freeddy Djiotsop <djiotsopfreeddy@gmail.com>
2023-10-17 22:45:10 +0200

    Unnötige Dateien löschen

--- client-code/src/components/user/passwort.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const PasswordEntcrypt = (pwd) => {
-  return bcrypt.hashSync(pwd, 10);
-};
-
-export const PasswordDecrypt = (pwd, hash) => {
-  return bcrypt.compareSync(pwd, hash);
-};
